721c734b4317952263f69cd130f6d81d99b64674
sdgandhi <sdgandhi@users.noreply.github.com>
2020-01-29 18:03:37 -0800

    Initial Commit

---
+++ catchup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  catchup
+//
+//  Created by Sidhant Gandhi on 1/29/20.
+//  Copyright © 2020 newnoetic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ catchupTests/catchupTests.swift
@@ -0,0 +1,34 @@
+//
+//  catchupTests.swift
+//  catchupTests
+//
+//  Created by Sidhant Gandhi on 1/29/20.
+//  Copyright © 2020 newnoetic. All rights reserved.
+//
+
+import XCTest
+@testable import catchup
+
+class catchupTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
