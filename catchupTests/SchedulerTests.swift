98cb386757c5036421c206f550259505b6a27ee3
sdgandhi <sdgandhi@users.noreply.github.com>
2020-02-05 17:25:14 -0800

    Create test for scheduler

---
+++ catchupTests/SchedulerTests.swift
@@ -0,0 +1,32 @@
+//
+//  SchedulerTests.swift
+//  catchupTests
+//
+//  Created by Sidhant Gandhi on 2/5/20.
+//  Copyright © 2020 newnoetic. All rights reserved.
+//
+
+import XCTest
+
+class SchedulerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testNextOpenSlot() {
+        
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
