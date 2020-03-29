fea36c72abc433654176169def1d214b02a79cec
sdgandhi <sdgandhi@users.noreply.github.com>
2020-03-28 23:57:49 -0700

    Simpler settings now persisted.

---
+++ catchup/utils/Settings.swift
@@ -0,0 +1,40 @@
+//
+//  Settings.swift
+//  catchup
+//
+//  Created by Sidhant Gandhi on 3/28/20.
+//  Copyright © 2020 newnoetic. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+final class Settings: ObservableObject {
+    let objectWillChange = PassthroughSubject<Void, Never>()
+    
+    @propertyWrapper
+    struct UserDefault<T> {
+        let key: String
+        let defaultValue: T
+        
+        init(_ key: String, defaultValue: T) {
+            self.key = key
+            self.defaultValue = defaultValue
+        }
+        
+        var wrappedValue: T {
+            get {
+                return UserDefaults.standard.object(forKey: key) as? T ?? defaultValue
+            }
+            set {
+                UserDefaults.standard.set(newValue, forKey: key)
+            }
+        }
+    }
+    
+    @UserDefault("settings.timeslotDuration", defaultValue: 30) var timeslotDuration: TimeInterval { willSet { objectWillChange.send() } }
+    @UserDefault("settings.weekdayTimelslotStartIndex", defaultValue: 17) var weekdayTimeslotStartIndex: Int { willSet { objectWillChange.send() } }
+    @UserDefault("settings.weekdayTimeslotEndIndex", defaultValue: 19) var weekdayTimeslotEndIndex: Int { willSet { objectWillChange.send() } }
+    @UserDefault("settings.weekendTimelslotStartIndex", defaultValue: 9) var weekendTimeslotStartIndex: Int { willSet { objectWillChange.send() } }
+    @UserDefault("settings.weekendTimeslotEndIndex", defaultValue: 21) var weekendTimeslotEndIndex: Int { willSet { objectWillChange.send() } }
+}
--- catchup/views/SettingsView.swift
+++ catchup/views/SettingsView.swift
@@ -9,41 +9,87 @@
 import SwiftUI
 import ContactsUI
 
+let calendar = Calendar(identifier: .gregorian)
+
 struct SettingsView: View {
+    
     @EnvironmentObject var upcoming: Upcoming
+    @ObservedObject var settings = Settings()
     @State private var showAlert: Bool = false
     @State private var alertMessage = ""
     @State private var showNewCatchupView: Bool = false
     @State private var catchup: Catchup? = nil
+
+//    @State private var settingsTimeslotDuration: TimeInterval = UserDefaults.standard.value(forKey: "settings.timeslotDuration") as? TimeInterval ?? 1800 {
+//        didSet {
+//            UserDefaults.standard.setValue(settingsTimeslotDuration, forKey: "settings.timeslotDuration")
+//        }
+//    }
+//
+//    @State private var settingsWeekdayTimeslotStartIndex: Int = UserDefaults.standard.value(forKey: "settings.weekdayTimelslotStartIndex") as? Int ?? 17 {
+//        didSet {
+//            UserDefaults.standard.setValue(settingsWeekdayTimeslotStartIndex, forKey: "settings.weekdayTimelslotStartIndex")
+//        }
+//    }
     
-    @State private var weekdayTimeslotStartIndex = 0
-    @State private var weekdayTimeslotEndIndex = 3
-    var weekdayTimeslotOptions = [0, 3600, 7200, 10800]
-    @State private var timeslotDurationIndex = 1
-    var timeslotDurationOptions = [900, 1800, 2700, 3600]
+    var timeslotOptions: [TimeInterval] = [3600, 3600*2, 3600*3, 3600*4, 3600*5, 3600*6, 3600*7, 3600*8, 3600*9, 3600*10, 3600*11, 3600*12, 3600*13, 3600*14, 3600*15, 3600*16, 3600*17, 3600*18, 3600*19, 3600*20, 3600*21, 3600*22, 3600*23]
+    
+    func timeslotToHour(timeslot: TimeInterval) -> String {
+        guard var today = calendar.date(bySettingHour: 0, minute: 0, second: 0, of: Date()) else {
+            return "\(timeslot)"
+        }
+        today.addTimeInterval(timeslot)
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateStyle = .none
+        return formatter.string(from: today)
+    }
     
     var body: some View {
         GeometryReader { geometry in
             Form {
                 Section(header: Text("Timeslot duration")) {
-                    Picker("Duration", selection: self.$timeslotDurationIndex) {
-                        ForEach(0 ..< self.timeslotDurationOptions.count) { index in
-                            Text("\(self.timeslotDurationOptions[index] / 60) minutes")
-                                .tag(index)
+                    VStack {
+                        Slider(value: self.$settings.timeslotDuration, in: 900...3600, step: 300)
+                        Text("\(Int(self.settings.timeslotDuration/60)) minutes")
+                    }.padding()
+                }
+                Section(header: Text("Weekday time slots")) {
+                    Picker("Start", selection: self.$settings.weekdayTimeslotStartIndex) {
+                        ForEach(0 ..< self.timeslotOptions.count) { index in
+                            Text("\(self.timeslotToHour(timeslot: self.timeslotOptions[index]))")
+                        }
+                    }
+                    Picker("End", selection: self.$settings.weekdayTimeslotEndIndex) {
+                        ForEach(0 ..< self.timeslotOptions.count) { index in
+                            Text("\(self.timeslotToHour(timeslot: self.timeslotOptions[index]))")
                         }
                     }
                 }
-                Section(header: Text("Weekday time slots")) {
-                    Picker("Start", selection: self.$weekdayTimeslotStartIndex) {
-                        ForEach(0 ..< self.weekdayTimeslotOptions.count) { index in
-                            Text("\(self.weekdayTimeslotOptions[index])")
-                                .tag(index)
+                Section(header: Text("Weekend time slots")) {
+                    Picker("Start", selection: self.$settings.weekendTimeslotStartIndex) {
+                        ForEach(0 ..< self.timeslotOptions.count) { index in
+                            Text("\(self.timeslotToHour(timeslot: self.timeslotOptions[index]))")
                         }
                     }
-                    Picker("End", selection: self.$weekdayTimeslotEndIndex) {
-                        ForEach(0 ..< self.weekdayTimeslotOptions.count) { index in
-                            Text("\(self.weekdayTimeslotOptions[index])")
-                                .tag(index)
+                    Picker("End", selection: self.$settings.weekendTimeslotEndIndex) {
+                        ForEach(0 ..< self.timeslotOptions.count) { index in
+                            Text("\(self.timeslotToHour(timeslot: self.timeslotOptions[index]))")
+                        }
+                    }
+                }
+                Section(footer: Text("Re-schedule to use new duration and time slot settings.")) {
+                    Button("Re-schedule catchups") {
+                        let allCatchups = (try? Database.shared.allCatchups()) ?? []
+                        Scheduler.shared.reschedule(allCatchups)
+                        .then { scheduledOrError in
+                                try scheduledOrError.compactMap { $0.value }.forEach { try Database.shared.upsert(catchup: $0) }
+                                scheduledOrError.compactMap { $0.error }.forEach { print($0.localizedDescription) } // TODO: grab individual errors and catchups from them if provided
+                        }
+                        .catch { error in
+                            print("could not reschedule some or all catchups")
+                            self.alertMessage = "Could not reschedule some or all catchups"
+                            self.showAlert = true
                         }
                     }
                 }
